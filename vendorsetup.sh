1417c4914190df9459a3b714a5e8539b72ef08be
omnitrix21 <okand7946@gmail.com>
2024-07-17 08:34:16 +0530

    sweet: track deps from vendorsetup.sh
    Signed-off-by: omnitrix21 <okand7946@gmail.com>

---
+++ vendorsetup.sh
@@ -0,0 +1,4 @@
+git clone https://github.com/omnitrix21/vendor_xiaomi_sweet vendor/xiaomi/sweet
+git clone https://github.com/omnitrix21/kernel_xiaomi_sweet kernel/xiaomi/sweet --depth=1
+git clone https://github.com/PixelOS-Devices/vendor_xiaomi_sweet-miuicamera vendor/xiaomi/sweet-miuicamera
+git clone https://github.com/TogoFire/packages_apps_ViPER4AndroidFX  packages/apps/ViPER4AndroidFX
