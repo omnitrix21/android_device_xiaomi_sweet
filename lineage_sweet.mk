15683b7313958451e99e5cfd0f961d646b2fedb1
omnitrix21 <okand7946@gmail.com>
2024-07-18 16:48:34 +0000

    sweet: correct maintainers name assignation
    Signed-off-by: omnitrix21 <okand7946@gmail.com>

--- lineage_sweet.mk
+++ lineage_sweet.mk
@@ -36,7 +36,6 @@ PRODUCT_GMS_CLIENTID_BASE := android-xiaomi
 
 BUILD_FINGERPRINT := Redmi/sweet_global/sweet:13/TKQ1.221013.002/V14.0.9.0.TKFMIXM:user/release-keys
 PRODUCT_BUILD_PROP_OVERRIDES += \
-    PRIVATE_BUILD_DESC="sweet_global-user 13 TKQ1.221013.002 V14.0.9.0.TKFMIXM release-keys"
-
-#Maintainer
-RISING_MAINTAINER="Aarav&Ajit"
+    PRIVATE_BUILD_DESC="sweet_global-user 13 TKQ1.221013.002 V14.0.9.0.TKFMIXM release-keys" \
+    TARGET_PRODUCT="sweet" \
+    RISING_MAINTAINER="Aarav&Ajit"
