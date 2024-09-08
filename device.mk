437f862089dcac6070d8c255c1f7b990ac0b0c20
omnitrix21 <okand7946@gmail.com>
2024-09-08 11:32:47 +0000

    Revert "sweet: Link Dolby Atmos dependencies against v33 libstagefright_foundation"
    - Shifted to hardware/lineage/compat
    
    This reverts commit fe9801e996dabb28b387e5515f5be366e797b759.
    
    Signed-off-by: omnitrix21 <okand7946@gmail.com>

--- device.mk
+++ device.mk
@@ -334,7 +334,6 @@ PRODUCT_PACKAGES += \
     libOmxVdec \
     libOmxVenc \
     libstagefrighthw \
-    libstagefright_foundation-v33 \
     libavservices_minijail_vendor \
     libcodec2_soft_common.vendor \
     libsfplugin_ccodec_utils.vendor
