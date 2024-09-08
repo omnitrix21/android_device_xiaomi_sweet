5b216e5a4c32b8fcbfbc5ffc6a34fb9a8e6db402
omnitrix21 <okand7946@gmail.com>
2024-09-08 11:12:09 +0000

    sweet: track our own fork of hardware/xiaomi
    Signed-off-by: omnitrix21 <okand7946@gmail.com>

--- vendorsetup.sh
+++ vendorsetup.sh
@@ -1,3 +1,4 @@
+git clone https://github.com/omnitrix21/hardware_xiaomi hardware/xiaomi
 git clone https://github.com/omnitrix21/vendor_xiaomi_sweet vendor/xiaomi/sweet
 git clone https://github.com/omnitrix21/kernel_xiaomi_sweet kernel/xiaomi/sweet --depth=1
 git clone https://gitlab.com/unmoved21/sweet-miuicamera vendor/xiaomi/sweet-miuicamera
