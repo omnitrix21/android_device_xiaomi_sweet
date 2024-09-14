cfec43590843e1531516990bf23228eca7b99a55
omnitrix21 <okand7946@gmail.com>
2024-09-14 05:42:05 +0000

    sweet: switch to another kernel fork
    Signed-off-by: omnitrix21 <okand7946@gmail.com>

--- vendorsetup.sh
+++ vendorsetup.sh
@@ -1,5 +1,5 @@
 git clone https://github.com/omnitrix21/hardware_xiaomi hardware/xiaomi
 git clone https://github.com/omnitrix21/vendor_xiaomi_sweet vendor/xiaomi/sweet
-git clone https://github.com/omnitrix21/kernel_xiaomi_sweet kernel/xiaomi/sweet --depth=1
+git clone https://github.com/unmoved21/kernel_xiaomi_sweet kernel/xiaomi/sweet --depth=1
 git clone https://gitlab.com/unmoved21/sweet-miuicamera vendor/xiaomi/sweet-miuicamera
 git clone https://github.com/TogoFire/packages_apps_ViPER4AndroidFX  packages/apps/ViPER4AndroidFX
